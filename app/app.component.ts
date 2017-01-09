2b26f33cd1d92047555aabaecc3865bd2c0abf8c
Synkopat <synkopeia@gmail.com>
2017-01-08 23:21:15 -0500

    Updating Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>Skeleton Project</h1>'
+    template: '<h1>Angular 2 Skeleton</h1>'
 })
 
 export class AppComponent {}
